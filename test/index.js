8e1902940ad068413a952b4839b46fa0386979c0
andypliang <andypliang@wesure.cn>
2018-12-21 11:51:44 +0800

    update test

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
  * @Author: andypliang 
  * @Date: 2018-12-18 17:16:16 
  * @Last Modified by: andypliang
- * @Last Modified time: 2018-12-19 16:43:42
+ * @Last Modified time: 2018-12-21 11:41:20
  */
 
 const path = require('path');
@@ -11,13 +11,13 @@ const svg2sprite = require('../index');
 svg2sprite({
     src: path.join(__dirname, 'svg'),
     dest: path.join(__dirname, 'dest'),
-    spriteName: 'testSprite',
-    useTimestamp: false,
-    noSizeStyle: false,
+    spriteName: 'ws-iconsprite',
+    useTimestamp: true,
+    noSizeStyle: true,
     publishConfig: {  // 文件发布配置
         remoteDomain: '', // 服务器域名，eg：https://cdn.com/
         remotePath: '', // 文件发布远程目录路径，eg: xxx/xxx/
-        config: { // 腾讯云对象存储配置，说明文档：https://cloud.tencent.com/document/product/436/8629
+        config: {
             AppId: '',
             SecretId: '',
             SecretKey: '',
